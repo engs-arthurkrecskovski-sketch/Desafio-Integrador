85270346f3376c35660b7ed7a2aaad89a6af6703
thiago <engs-thiagoireno@camporeal.edu.br>
2026-05-24 02:00:49 -0300

    foi criado o enum Categoria com metodo fromString

--- Model/Categoria.java
+++ Model/Categoria.java
@@ -1,5 +1,12 @@
 package model;
 
-public class Categoria {
-    
+public enum Categoria {
+    ALIMENTOS, ELETRONICOS, LIVROS;
+
+    public static Categoria fromString(String valor) {
+        for (Categoria c : values()) {
+            if (c.name().equalsIgnoreCase(valor.trim())) return c;
+        }
+        throw new IllegalArgumentException("Categoria nvalida: " + valor);
+    }
 }
