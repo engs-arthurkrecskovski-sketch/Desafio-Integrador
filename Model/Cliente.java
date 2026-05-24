8074735e4be7e2afcb464fd81fd649d61c610fc2
thiago <engs-thiagoireno@camporeal.edu.br>
2026-05-24 01:35:45 -0300

    Dentro da classe Cliente colocando os atributos imutáveis e construtores

--- Model/Cliente.java
+++ Model/Cliente.java
@@ -1,5 +1,24 @@
-package Model;
+package model;
 
-public class Cliente {
-    
+import java.time.LocalDateTime;
+
+public final class Cliente {
+
+    private final long id;
+    private final String nome;
+    private final String email;
+    private final LocalDateTime criadoEm;
+
+    public Cliente(String nome, String email) {
+        this(0L, nome, email,LocalDateTime.now());
+    }
+
+     public Cliente(long id, String nome, String email, LocalDateTime criadoEm) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.criadoEm = criadoEm;
+    }
+
 }
+
