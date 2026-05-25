c1990b03ef038ad94658a07d081e5e8fb6c8e262
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-05-25 13:32:18 -0300

    inicio do codigo item pedido

---
+++ src/com/sistema/model/ItemPedido.java
@@ -0,0 +1,8 @@
+package com.sistema.model;
+
+public final class ItemPedido {
+
+    private final long id;
+    private final Produto produto;
+    private final int quantidade;
+    private final double precoUnitario;
