5d0b43c5f32ec66d477335ddce91583bfbdaa34c
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-05-25 14:19:17 -0300

    inicio da contrução da classe pedido

--- src/com/sistema/model/Pedido.java
+++ src/com/sistema/model/Pedido.java
@@ -4,3 +4,16 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+public final class Pedido {
+
+    private final long id;
+    private final Cliente cliente;
+    private final List<ItemPedido> itens;
+    private StatusPedido status;
+    private final LocalDateTime criadoEm;
+
+    public Pedido(Cliente cliente) {
+        if (cliente == null) {
+            throw new IllegalArgumentException("Um pedido precisa estar associado a um cliente válido.");
+        }
