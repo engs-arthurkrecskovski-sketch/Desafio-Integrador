c9977b432c34b7a37c931881cdc0b177cb0652b3
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-05-27 08:03:26 -0300

    Inicio da classe produtoDao

---
+++ src/com/sistema/dao/ProdutoDao.java
@@ -0,0 +1,11 @@
+package com.sistema.dao;
+
+import com.sistema.model.Categoria;
+import com.sistema.model.Produto;
+import com.sistema.util.ConnectionUtil;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
