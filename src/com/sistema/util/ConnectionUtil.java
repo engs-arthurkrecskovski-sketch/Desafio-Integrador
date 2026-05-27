a04f99cd0c6885845aaf33906177a64f2029289b
Bruno <engs-brunoroth@camporeal.edu.br>
2026-05-27 06:58:24 -0300

    correção de erros conection 1

--- src/com/sistema/util/ConnectionUtil.java
+++ src/com/sistema/util/ConnectionUtil.java
@@ -5,9 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionUtil {
-}
-   
-private static final String URL =
+
+    private static final String URL =
         "jdbc:mysql://localhost:3306/sistema_pedidos?useSSL=false&serverTimezone=America/Sao_Paulo&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
     private static final String PASS = "root";
@@ -17,4 +16,5 @@ private static final String URL =
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
+}
 
