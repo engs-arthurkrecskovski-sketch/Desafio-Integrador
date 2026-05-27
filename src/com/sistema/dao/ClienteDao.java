3fa5d29433df46a57937b06ef88d937e7db05805
Bruno <engs-brunoroth@camporeal.edu.br>
2026-05-27 07:09:58 -0300

    adiciona imports necessários

--- src/com/sistema/dao/ClienteDao.java
+++ src/com/sistema/dao/ClienteDao.java
@@ -3,3 +3,12 @@ package com.sistema.dao;
 public class ClienteDAO {
 
 }
+
+import com.sistema.model.Cliente;
+import com.sistema.util.ConnectionUtil;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
