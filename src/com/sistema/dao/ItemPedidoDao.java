c8d47a12de96a4d051bd9e37849737e4af5ecb83
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-05-27 10:45:01 -0300

    item pedidao inicio

---
+++ src/com/sistema/dao/ItemPedidoDao.java
@@ -0,0 +1,10 @@
+package com.sistema.dao;
+
+import com.sistema.model.Categoria;
+import com.sistema.model.ItemPedido;
+import com.sistema.model.Produto;
+import com.sistema.util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
