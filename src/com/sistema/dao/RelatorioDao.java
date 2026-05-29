69a5a8344cde9bc65323cb1b58e25961fb8231ba
Murilo <murilogrando321@gmail.com>
2026-05-29 08:59:01 -0300

    criando classe RelatorioDAO

---
+++ src/com/sistema/dao/RelatorioDao.java
@@ -0,0 +1,11 @@
+package com.sistema.dao;
+
+import com.sistema.util.ConectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RelatorioDAO {
+    
+}
