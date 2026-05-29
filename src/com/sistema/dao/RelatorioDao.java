1ad9934cf1c0dbc1bbd1714c6affe2b87a894e8b
Murilo <murilogrando321@gmail.com>
2026-05-29 09:57:51 -0300

    tabela principal da consulta

--- src/com/sistema/dao/RelatorioDao.java
+++ src/com/sistema/dao/RelatorioDao.java
@@ -10,5 +10,7 @@ public class RelatorioDAO {
     
      public List<String> relatorioTopClientes(int limite) throws SQLException {
         String sql = "SELECT c.nome, COUNT(p.id) AS total_pedidos, " +
-             "SUM(ip.quantidade * ip.preco_unit) AS valor_total " +
+            "SUM(ip.quantidade * ip.preco_unit) AS valor_total " +
+            "FROM clientes c " +
+
 }
