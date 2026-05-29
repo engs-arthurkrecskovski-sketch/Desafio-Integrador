f0af085a928bb5e39e433edd66145e3f57b3176e
Murilo <murilogrando321@gmail.com>
2026-05-29 10:07:04 -0300

    agrupameto de resultados por cliente

--- src/com/sistema/dao/RelatorioDao.java
+++ src/com/sistema/dao/RelatorioDao.java
@@ -15,5 +15,5 @@ public class RelatorioDAO {
             "JOIN pedidos p ON p.cliente_id = c.id " +
             "JOIN itens_pedido ip ON ip.pedido_id = p.id " +
             "WHERE p.status = 'FINALIZADO' " +
-
+            "GROUP BY c.id, c.nome 
 }
