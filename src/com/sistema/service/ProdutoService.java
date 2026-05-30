469ea46786328c451f92c512400a7f22105f0e22
Murilo <murilogrando321@gmail.com>
2026-05-30 15:31:54 -0300

    criando ProdutoService e adicionando os imports necessarios

---
+++ src/com/sistema/service/ProdutoService.java
@@ -0,0 +1,15 @@
+package com.sistema.service;
+
+import com.sistema.dao.ProdutoDAO;
+import com.sistema.exception.EntidadeNaoEncontradaException;
+import com.sistema.exception.ValidacaoException;
+import com.sistema.model.Categoria;
+import com.sistema.model.Produto;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProdutoService {
+    
+}
