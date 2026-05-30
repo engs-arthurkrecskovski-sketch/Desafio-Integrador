62fef61e44b1f5b35220e600761f7b950cd68f67
Murilo <murilogrando321@gmail.com>
2026-05-30 14:53:06 -0300

    criando ClienteService e adicionando imports necessarios

--- src/com/sistema/service/ClienteService.java
+++ src/com/sistema/service/ClienteService.java
@@ -1,5 +1,15 @@
 package com.sistema.service;
 
+import com.sistema.dao.ClienteDAO;
+import com.sistema.exception.EmailInvalidoException;
+import com.sistema.exception.EntidadeNaoEncontradaException;
+import com.sistema.exception.ValidacaoException;
+import com.sistema.model.Cliente;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class ClienteService {
     
 }
