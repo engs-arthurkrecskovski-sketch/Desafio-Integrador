add39935aad3ff0861dae2c06482d4b263ded223
thiago <engs-thiagoireno@camporeal.edu.br>
2026-05-31 18:27:47 -0300

    Criacao na classe RelatorioService com metodos de estatisticas

--- src/com/sistema/service/RelatorioService.java
+++ src/com/sistema/service/RelatorioService.java
@@ -5,7 +5,23 @@ import com.sistema.dao.RelatorioDAO;
 import java.sql.SQLException;
 import java.util.List;
 
-
 public class RelatorioService {
     
+    private final RelatorioDAO relatorioDAO = new RelatorioDAO();
+
+    public List<String> topClientes(int limite) throws SQLException{
+        return relatorioDAO.relatorioTopClientes(limite);
+    }
+
+    public List<String> produtosMaisVendidos(int limite) throws SQLException{
+        return relatorioDAO.relatorioProdutosMaisVendidos(limite);
+    }
+
+    public List<String> pedidosPorStatus() throws SQLException {
+        return relatorioDAO.relatorioPedidosporStatus();
+    }
+
+    public List<String> estoqueCritico(int limiteEstoque) throws SQLException {
+        return relatorioDAO.relatorioEstoqueCritico(limiteEstoque);
+    }
 }
