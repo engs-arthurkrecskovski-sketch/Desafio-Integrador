d34d8aaea621edf213e636f1d73adcf031e5cb7d
Bruno <engs-brunoroth@camporeal.edu.br>
2026-06-01 13:26:55 -0300

    Criar pacote e imports

--- src/com/sistema/thread/OrdemProcesso.java
+++
@@ -1,5 +0,0 @@
-package com.sistema.thread;
-
-public class OrdemProcesso {
-    
-}
---
+++ src/com/sistema/thread/OrderProcessor.java
@@ -0,0 +1,7 @@
+package com.sistema.thread;
+
+import com.sistema.dao.PedidoDAO;
+import com.sistema.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
