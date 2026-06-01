b93f0a961863aa7fb0c25c37cdcb69b610c31d18
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-06-01 11:07:10 -0300

    alterações na main

--- src/com/sistema/Main.java
+++ src/com/sistema/Main.java
@@ -6,7 +6,7 @@ import com.sistema.console.ProdutoMenu;
 import com.sistema.console.RelatorioMenu;
 import com.sistema.thread.OrderProcessor;
 
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Main {
 
