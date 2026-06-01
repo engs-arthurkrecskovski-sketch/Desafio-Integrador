ee02e324d00d1d1e95802dea9e0b835cac6615c2
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-06-01 11:01:26 -0300

    inicio da main

--- src/com/sistema/Main.java
+++ src/com/sistema/Main.java
@@ -1,1 +1,9 @@
+package com.sistema;
 
+import com.sistema.console.ClienteMenu;
+import com.sistema.console.PedidoMenu;
+import com.sistema.console.ProdutoMenu;
+import com.sistema.console.RelatorioMenu;
+import com.sistema.thread.OrderProcessor;
+
+import java.util.Scanner
